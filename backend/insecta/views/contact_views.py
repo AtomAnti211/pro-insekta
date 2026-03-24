fc3c1ee1e7cbe214237e66912ac7dd81ec29ba7c
Edit968 <bedit968@gmail.com>
2026-03-24 08:22:34 +0100

    Correction of htmel of email:

--- backend/insecta/views/contact_views.py
+++ backend/insecta/views/contact_views.py
@@ -22,10 +22,50 @@ def contactMessageCreate(request):
         text_content = "Köszönjük a megkeresést."
 
         html_content = f"""
+        
         <html>
+        <head>
+            <style>
+                body {{
+                    font-family: Georgia, 'Times New Roman', serif;
+                    color: #333;
+                }}
+                .header {{
+                    text-align: center;
+                    margin-bottom: 20px;
+                }}
+                .logo {{
+                    width: 180px;
+                }}
+                .highlight {{
+                    color: #c62828;
+                }}
+            </style>
+        </head>
         <body>
-            <p>Tisztelt {message.name}!</p>
-            <p>Köszönettel vettük megkeresését.</p>
+
+            <p>Tisztelt <span class="highlight">{message.name}</span>!</p>
+
+            <p>
+                Köszönettel vettük megkeresését kártevőmentesítéssel kapcsolatban.
+                Kollégáink hamarosan felveszik Önnel a kapcsolatot a megadott elérhetőségeken.
+            </p>
+
+            <p><strong>Név:</strong> {message.name}<br>
+            <strong>Email:</strong> {message.email}<br>
+            <strong>Telefon:</strong> {message.phone}</p>
+            <strong>Cím:</strong> {message.address}</p>
+            <p><strong>Érdeklődési kör:</strong> {message.activity.activityName if message.activity else 'N/A'}</p>
+            <p><strong>Üzenet:</strong><br>{message.message}</p>
+
+            <br>
+            <p>Üdvözlettel,<br>
+            <strong>Insecta Kft.</strong><br>
+            Mekk Elek<br>
+            ügyvezető</p>
+            <div class="footer">
+                <img src="cid:company_logo" class="logo">
+            </div>
         </body>
         </html>
         """
